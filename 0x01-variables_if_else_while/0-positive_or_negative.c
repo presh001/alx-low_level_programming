51584f39f920ee2c7198864da74cf7b6aa69400c
Super User <precyomidiji@gmail.com>
2022-03-12 11:32:24 -0800

    last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@
  *
  *    * and if it is positive, negative, or zero
  *
- *     * Return: 0
+ *     * Return: 
  *
  *      */
 
@@ -23,25 +23,11 @@ int main(void)
 {
 
 		int n;
+          
+		srand(time(0))
+		n = rand() - RAND_MAX / 2;
 
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-
-
-					if (n > 0)
-
-								printf("%i is positive\n", n);
-
-						else if (n < 0)
-
-									printf("%i is negative\n", n);
-
-							else
-
+          	if (n > 0)						  printf("%i is 
 										printf("%i is zero\n", n);
 
 
@@ -49,5 +35,4 @@ int main(void)
 								return (0);
 
 }
-
 
