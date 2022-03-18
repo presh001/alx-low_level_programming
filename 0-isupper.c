1cda3e415f46d3b02e22c93f010e9f4e866d1c2a
Super User <precyomidiji@gmail.com>
2022-03-18 14:26:38 -0700

    supper

---
+++ 0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: Variable text
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return(i);
+	}
+	else
+	{
+		return (0);
+	}
+}
